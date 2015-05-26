73a62190b835d90ef5ad852f12b0e4434d70db52
Drew Machat <drew@alleyinteractive.com>
2015-05-25 22:32:42 -0400

    Minified for release

--- dist/angular-datamaps.js
+++ dist/angular-datamaps.js
@@ -1,24 +1,18 @@
 'use strict';
-
 angular.module('datamaps', []);
-
 'use strict';
-
-angular
-
-  .module('datamaps')
-
-  .directive('datamap', ['$window', function($window) {
+angular.module('datamaps').directive('datamap', [
+  '$window',
+  function ($window) {
     return {
       restrict: 'EA',
       scope: {
-        map: '=',       //datamaps objects [required]
-        plugins: '=?',  //datamaps plugins [optional]
-        zoomable: '@?', //zoomable toggle [optional]
-        onClick: '&?',  //geography onClick event [optional]
+        map: '=',
+        plugins: '=?',
+        zoomable: '@?',
+        onClick: '&?'
       },
-      link: function(scope, element, attrs) {
-
+      link: function (scope, element, attrs) {
         // Generate base map options
         function mapOptions() {
           return {
@@ -26,86 +20,64 @@ angular
             scope: 'usa',
             height: scope.height,
             width: scope.width,
-            fills: {
-              defaultFill: '#b9b9b9'
-            },
+            fills: { defaultFill: '#b9b9b9' },
             data: {},
-            done: function(datamap) {
+            done: function (datamap) {
               function redraw() {
-                datamap.svg.selectAll('g')
-                  .attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
+                datamap.svg.selectAll('g').attr('transform', 'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')');
               }
               if (angular.isDefined(attrs.onClick)) {
-                datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+                datamap.svg.selectAll('.datamaps-subunit').on('click', function (geography) {
                   scope.onClick()(geography);
                 });
               }
               if (angular.isDefined(attrs.zoomable)) {
-                datamap.svg.call(d3.behavior.zoom()
-                  .on('zoom', redraw));
+                datamap.svg.call(d3.behavior.zoom().on('zoom', redraw));
               }
             }
           };
         }
-
         scope.api = {
-
-          // Fully refresh directive
-          refresh: function(map) {
+          refresh: function (map) {
             scope.api.updateWithOptions(map);
           },
-
-          // Update chart with new options
-          updateWithOptions: function(map) {
-
+          updateWithOptions: function (map) {
             // Clearing
             scope.api.clearElement();
-
             // Update bounding box
             scope.width = (map.options || {}).width || null;
             scope.height = (map.options || {}).height || (scope.width ? scope.width * 0.5 : null);
             scope.legendHeight = (map.options || {}).legendHeight || 50;
-
             // Set a few defaults for the directive
             scope.mapOptions = mapOptions();
-
             // Add the good stuff
             scope.mapOptions = angular.extend(scope.mapOptions, map);
-
             scope.datamap = new Datamap(scope.mapOptions);
-
             // Add responsive listeners
             if (scope.mapOptions.responsive) {
               $window.addEventListener('resize', scope.api.resize);
             } else {
               $window.removeEventListener('resize', scope.api.resize);
             }
-
             // Update plugins
             scope.api.updatePlugins(scope.datamap);
-
             // Update options and choropleth
             scope.api.refreshOptions(map.options);
             scope.api.updateWithData(map.data);
           },
-
-          // Add and initialize optional plugins
-          updatePlugins: function(datamap) {
+          updatePlugins: function (datamap) {
             if (!scope.plugins) {
               return;
             }
-            angular.forEach(scope.plugins, function(plugin, name) {
+            angular.forEach(scope.plugins, function (plugin, name) {
               datamap.addPlugin(name, plugin);
               datamap[name]();
             });
           },
-
-          // Set options on the datamap
-          refreshOptions: function(options) {
+          refreshOptions: function (options) {
             if (!options) {
               return;
             }
-
             // set labels
             if (options.labels) {
               scope.datamap.labels({
@@ -113,40 +85,30 @@ angular
                 fontSize: options.labelSize ? options.labelSize : 12
               });
             }
-
             // set legend
             if (options.legend) {
               scope.datamap.legend();
             }
           },
-
-          // Trigger datamaps resize method
-          resize: function() {
+          resize: function () {
             console.log('resize attempt');
             scope.datamap.resize();
           },
-
-          // Update chart with new data
-          updateWithData: function(data) {
+          updateWithData: function (data) {
             scope.datamap.updateChoropleth(data);
             scope.api.updatePlugins(scope.datamap);
           },
-
-          // Fully clear directive element
           clearElement: function () {
             scope.datamap = null;
-            element
-              .empty()
-              .css({
-                'position': 'relative',
-                'display': 'block',
-                'padding-bottom': scope.legendHeight + 'px'
-              });
+            element.empty().css({
+              'position': 'relative',
+              'display': 'block',
+              'padding-bottom': scope.legendHeight + 'px'
+            });
           }
         };
-
         // Watch data changing
-        scope.$watch('map', function(map, old) {
+        scope.$watch('map', function (map, old) {
           // Return if no data
           if (!map || angular.equals({}, map)) {
             return;
@@ -163,4 +125,5 @@ angular
         }, true);
       }
     };
-  }]);
+  }
+]);
